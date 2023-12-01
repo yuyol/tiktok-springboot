eed6021aebb4dcd2b5def81331a4aad207faf4ed
Yu Yang <ylctyuy@gmail.com>
2023-11-30 23:36:15 -0800

    add vue-based frontend project

---
+++ web/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Index></Index>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Index from "./components/Index.vue";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld
+    Index
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
