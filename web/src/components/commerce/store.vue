f0b8f116fe394f321cd65942824fb119913d2799
Yu Yang <ylctyuy@gmail.com>
2023-12-30 22:09:54 -0800

    添加商城页面

---
+++ web/src/components/commerce/store.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+      merchantInfo: [
+        {
+          id: 1,
+          title: "",
+          description: "",
+          price: 500,
+          rating: 4.5,
+        },
+      ],
+    };
+  },
+};
+</script>
